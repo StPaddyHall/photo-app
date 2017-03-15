82b8218af8b3aacb76b61db5213ba224a12b40cf
Patrick <artificial.kulture@gmail.com>
2017-03-15 06:06:39 +0000

    debugging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  
+  puts "SENDGRID_USERNAME: #{ENV['SENDGRID_USERNAME']}"
+  puts "SENDGRID_PASSWORD: #{ENV['SENDGRID_PASSWORD']}"
 end
